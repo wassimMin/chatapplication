3e81984cdcf3d01a816cfcd077eb8ab8673e3437
Younsi wassim <120186938+wassimMin@users.noreply.github.com>
2024-07-03 11:56:55 +0100

    www

--- backend/fetch_users.php
+++ backend/fetch_users.php
@@ -1,8 +1,15 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require 'db.php';
 session_start();
+if (!isset($_SESSION['userid'])) {
+    die("User not logged in");
+}
 
-if(isses($_SESSION['userid'])){
+if(isset($_SESSION['userid'])){
     $current_user = $_SESSION['userid'];
     $stmt = $conn->prepare("SELECT userid,name FROM users WHERE userid != ?");
     $stmt->bind_param("i",$current_user);
